97b57a3d8b3f483381c0bf51997dae317dad1dc1
thejunghare <prasadjunghare2@gmail.com>
2024-05-13 18:51:57 +0530

    ui update

--- screens/WelcomeScreen.tsx
+++ screens/WelcomeScreen.tsx
@@ -1,20 +1,28 @@
 import * as React from 'react';
-import { SafeAreaView } from 'react-native';
-import { Button, Text } from 'react-native-paper'
+import { View, Image } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { Button } from 'react-native-paper'
 import { useNavigation } from '@react-navigation/native';
 
 const WelcomeScreen = () => {
     const navigation = useNavigation();
 
     return (
-        <SafeAreaView className='flex-1'>
-            <Button
-                mode='outlined'
-                icon='arrow-right'
-                onPress={() => navigation.replace('Login')}
-            >
-                Next
-            </Button>
+        <SafeAreaView className='h-screen flex-1'>
+            <View className='h-1/2 m-auto flex items-center justify-around'>
+                <Image
+                    source={require('../assets/digital-neta-logo.png')}
+                    className='w-52 h-14'
+                />
+                <Button
+                    mode='contained'
+                    buttonColor='#5783B9'
+                    icon='arrow-right'
+                    compact={true}
+                    onPress={() => navigation.navigate('Login')}
+                >
+                </Button>
+            </View>
         </SafeAreaView>
     );
 }

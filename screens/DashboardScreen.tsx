41c73f90e166c94e354d1e45fe03355f942e228f
thejunghare <prasadjunghare2@gmail.com>
2024-05-15 17:54:51 +0530

    update username dynamically

--- screens/DashboardScreen.tsx
+++ screens/DashboardScreen.tsx
@@ -1,13 +1,38 @@
-import * as React from 'react';
+import React, { useEffect, useState } from "react";
 import { SafeAreaView, View } from 'react-native';
 import { Text, Avatar, Icon } from 'react-native-paper';
+import { firebase, auth } from '../firebase';
 
 const DashboardScreen = () => {
+    const [currentEmployee, setCurrentEmployee] = useState(null);
+
+    useEffect(() => {
+        const fetchUserDetails = async () => {
+            const employee = firebase.auth().currentUser;
+
+            if (employee) {
+                const userRef = firebase.firestore().collection('employee').doc(employee.uid);
+                try {
+                    const docSnapshot = await userRef.get();
+
+                    if (docSnapshot.exists) {
+                        setCurrentEmployee(docSnapshot.data());
+                    } else console.error('employee document do not exist')
+                }
+                catch (error) {
+                    console.error('error fetching employee: ', error);
+                }
+            }
+        }
+        fetchUserDetails();
+        return () => { }
+    });
+
     return (
         <SafeAreaView className='flex-1 h-screen'>
             <View className='h-2/4 bg-app-blue flex items-center justify-evenly'>
                 <Avatar.Image className='bg-transparent' size={200} source={require('../assets/noun-avatar.png')} />
-                <Text className='bg-white shadow-sm py-2 px-5 rounded-lg text-base'>Prasad Junghare</Text>
+                <Text className='bg-white shadow-sm py-2 px-5 rounded-lg text-base'>{currentEmployee && currentEmployee.fname} {currentEmployee && currentEmployee.lname}</Text>
             </View>
 
             <View className='h-2/4 mt-2'>

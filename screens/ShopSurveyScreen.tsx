078d44aed4f2c4d1c2b2e289e0796d8a9720b0e1
thejunghare <prasadjunghare2@gmail.com>
2024-05-16 15:20:08 +0530

    update shop survey logic to store data division and ward wise

--- screens/ShopSurveyScreen.tsx
+++ screens/ShopSurveyScreen.tsx
@@ -1,25 +1,85 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { ScrollView, View, Text, StyleSheet } from 'react-native';
-import { TextInput, Button, ActivityIndicator, Icon, MD3Colors } from 'react-native-paper';
+import { TextInput, Button, ActivityIndicator, Icon, Checkbox } from 'react-native-paper';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { firebase, auth } from '../firebase';
 import ActionSheet from "react-native-actions-sheet";
+import { Dropdown } from 'react-native-element-dropdown';
+
+interface DataItem {
+    label: string;
+    value: string;
+}
 
 const ShopSurveyScreen: React.FC = () => {
     const [shopName, setShopName] = useState<string>('');
     const [shopOwnerName, setShopOwnerName] = useState<string>('');
     const [shopPhoneNumber, setShopPhoneNumber] = useState<string>('');
+    const [division, setDivision] = useState<string>('');
+    const [ward, setWard] = useState<string>('');
     const [loading, setLoading] = useState<boolean>(false);
     const actionSheetRef = useRef<null>(null);
     const [show, setShow] = useState<boolean>(false);
+    const [isFocus, setIsFocus] = useState<boolean>(false);
+    const [checked, setChecked] = React.useState(false);
+    const [divisionData, setDivisionData] = useState<DataItem[]>([]);
+    const [wardData, setWardData] = useState<DataItem[]>([]);
+
+    useEffect(() => {
+        const fetchDivisionData = async () => {
+            try {
+                const divisionSnapshot = await employeeRef.doc(currentUser.uid).collection('divisions').get();
+                const divisionData: DataItem[] = divisionSnapshot.docs.map(doc => ({
+                    label: doc.data().name,
+                    value: doc.data()._id,
+                }));
+                setDivisionData(divisionData);
+            } catch (error) {
+                console.error("Error fetching division data: ", error);
+            }
+        };
+
+        const fetchWardData = async () => {
+            try {
+                const divisionSnapshot = await employeeRef.doc(currentUser.uid).collection('divisions').get();
+                const wardData: DataItem[] = [];
+
+                await Promise.all(divisionSnapshot.docs.map(async divisionDoc => {
+                    const wardSnapshot = await divisionDoc.ref.collection('wards').get();
+                    wardSnapshot.forEach(wardDoc => {
+                        wardData.push({
+                            label: wardDoc.data().name,
+                            value: wardDoc.data()._id,
+                        });
+                    });
+                }));
+
+                setWardData(wardData);
+            } catch (error) {
+                console.error("Error fetching ward data: ", error);
+            }
+        };
+
+        const currentUser = firebase.auth().currentUser;
+        const employeeRef = firebase.firestore().collection('employee');
+
+        if (currentUser) {
+            fetchDivisionData();
+            fetchWardData();
+        }
+    }, [firebase.auth().currentUser]);
+
+
 
     const handleShopNameChange = (value: string) => { setShopName(value) };
     const handleShopOwnerNameChange = (value: string) => { setShopOwnerName(value) };
     const handleShopPhoneNumberChange = (value: string) => { setShopPhoneNumber(value) };
+    const handleDivisionChange = (value: string) => { setDivision(value) };
+    const handleWardChange = (value: string) => { setWard(value) };
 
     const handleAddSurveyData = () => {
         if (!shopName && !shopOwnerName && !shopPhoneNumber) alert('* is Required field!')
-        console.log(shopName, shopOwnerName, shopPhoneNumber);
+        console.log(shopName, shopOwnerName, shopPhoneNumber, division, ward);
 
 
         const surveyInfo = {
@@ -29,7 +89,7 @@ const ShopSurveyScreen: React.FC = () => {
             shop_phone_number: shopPhoneNumber,
         };
 
-        if (shopName && shopOwnerName && shopPhoneNumber ) {
+        if (shopName && shopOwnerName && shopPhoneNumber) {
             addSurveyDataToFirestore(surveyInfo);
         }
     };
@@ -44,6 +104,10 @@ const ShopSurveyScreen: React.FC = () => {
                     .firestore()
                     .collection('employee')
                     .doc(currentEmployee.uid)
+                    .collection('divisions')
+                    .doc(division)
+                    .collection('wards')
+                    .doc(ward)
                     .collection('shop-survey');
 
                 await shopSurveyRef.add(surveyInfo);
@@ -66,11 +130,76 @@ const ShopSurveyScreen: React.FC = () => {
         setShopName('');
         setShopOwnerName('');
         setShopPhoneNumber('');
+        setDivision('');
+        setWard('');
     };
 
     return (
         <ScrollView>
             <View className='m-2'>
+                <View className='w-full flex flex-row justify-around'>
+                    <Dropdown
+                        style={[styles.dropdown, isFocus && { borderColor: 'black' }]}
+                        placeholderStyle={styles.placeholderStyle}
+                        selectedTextStyle={styles.selectedTextStyle}
+                        inputSearchStyle={styles.inputSearchStyle}
+                        iconStyle={styles.iconStyle}
+                        data={divisionData}
+                        search
+                        maxHeight={300}
+                        labelField="label"
+                        valueField="value"
+                        placeholder={!isFocus ? 'Select division' : '...'}
+                        searchPlaceholder="Search..."
+                        value={division}
+                        onFocus={() => setIsFocus(true)}
+                        onBlur={() => setIsFocus(false)}
+                        onChange={item => {
+                            setDivision(item.value);
+                            setIsFocus(false);
+                        }}
+                        renderLeftIcon={() => (
+                            <View className='mr-1.5'>
+                                <Icon
+                                    source="map-marker-outline"
+                                    size={20}
+                                />
+                            </View>
+
+                        )}
+                    />
+
+                    <Dropdown
+                        style={[styles.dropdown, isFocus && { borderColor: 'black' }]}
+                        placeholderStyle={styles.placeholderStyle}
+                        selectedTextStyle={styles.selectedTextStyle}
+                        inputSearchStyle={styles.inputSearchStyle}
+                        iconStyle={styles.iconStyle}
+                        data={wardData}
+                        search
+                        maxHeight={300}
+                        labelField="label"
+                        valueField="value"
+                        placeholder={!isFocus ? 'Select ward' : '...'}
+                        searchPlaceholder="Search..."
+                        value={ward}
+                        onFocus={() => setIsFocus(true)}
+                        onBlur={() => setIsFocus(false)}
+                        onChange={item => {
+                            setWard(item.value);
+                            setIsFocus(false);
+                        }}
+                        renderLeftIcon={() => (
+                            <View className='mr-1.5'>
+                                <Icon
+                                    source="map-marker-multiple-outline"
+                                    size={20}
+                                />
+                            </View>
+
+                        )}
+                    />
+                </View>
                 <TextInput
                     label='Shop name'
                     mode='outlined'
@@ -95,6 +224,17 @@ const ShopSurveyScreen: React.FC = () => {
                     className='m-2'
                     right={<TextInput.Icon icon="phone-outline" />}
                 />
+
+                <View className='w-full flex flex-row items-center p-3.5'>
+                    <Checkbox
+                        status={checked ? 'checked' : 'unchecked'}
+                        onPress={() => {
+                            setChecked(!checked);
+                        }}
+                    />
+                    <Text className='mx-2'>I affirm that all provided information is accurate, and there are no typos in the submitted form.</Text>
+                </View>
+
 
 
                 <Button
@@ -131,6 +271,29 @@ const styles = StyleSheet.create({
         padding: 5,
         alignItems: 'center',
         justifyContent: 'center'
+    },
+
+    dropdown: {
+        height: 50,
+        borderColor: 'gray',
+        borderWidth: 0.5,
+        borderRadius: 8,
+        paddingHorizontal: 5,
+        width: '48%',
+    },
+    placeholderStyle: {
+        fontSize: 16,
+    },
+    selectedTextStyle: {
+        fontSize: 16,
+    },
+    iconStyle: {
+        width: 20,
+        height: 20,
+    },
+    inputSearchStyle: {
+        height: 40,
+        fontSize: 16,
     },
 });
 

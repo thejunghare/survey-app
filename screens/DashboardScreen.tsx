7ec3b4ba1aebe603e079848533ec709501bee2f2
thejunghare <prasadjunghare2@gmail.com>
2024-05-16 19:13:30 +0530

    fix icons view and updated avatar image

--- screens/DashboardScreen.tsx
+++ screens/DashboardScreen.tsx
@@ -31,45 +31,49 @@ const DashboardScreen = () => {
     return (
         <SafeAreaView className='flex-1 h-screen'>
             <View className='h-2/4 bg-app-blue flex items-center justify-evenly'>
-                <Avatar.Image className='bg-transparent' size={200} source={require('../assets/noun-avatar.png')} />
+                <Avatar.Image className='bg-transparent' size={200} source={require('../assets/avatar.jpg')} />
                 <Text className='bg-white shadow-sm py-2 px-5 rounded-lg text-base'>{currentEmployee && currentEmployee.fname} {currentEmployee && currentEmployee.lname}</Text>
             </View>
 
             <View className='h-2/4 mt-2'>
+                <View className='bg-white p-3 m-5 rounded-lg'>
+                    <Text className='text-center font-bold text-base'>Statistics</Text>
+                </View>
+
                 <View className='flex flex-row items-center justify-around m-2'>
-                    <View className='flex items-center justify-evenly bg-white rounded-lg shadow-sm p-2.5'>
+                    <View className='w-5/12 flex items-center justify-evenly bg-white rounded-lg shadow-sm p-2.5'>
                         <Icon size={40} source={require('../assets/shop-50.png')} />
-                        <View className='flex items-center'>
-                            <Text className='font-bold text-base'>477</Text>
-                            <Text>Shop survey</Text>
+                        <View className='mt-2 flex items-center'>
+                            <Text className='font-bold text-lg'>477</Text>
+                            <Text className='font-semibold text-base'>Shop survey</Text>
                         </View>
                     </View>
 
 
-                    <View className='flex items-center justify-evenly bg-white rounded-lg shadow-sm p-2.5'>
+                    <View className='w-5/12 flex items-center justify-evenly bg-white rounded-lg shadow-sm p-2.5'>
                         <Icon size={40} source={require('../assets/human.png')} />
-                        <View className='flex items-center'>
-                            <Text className='font-bold text-base'>157</Text>
-                            <Text>People survey</Text>
+                        <View className='mt-2 flex items-center'>
+                            <Text className='font-bold text-lg'>157</Text>
+                            <Text className='font-semibold text-base'>People survey</Text>
                         </View>
                     </View>
                 </View>
 
                 <View className='flex flex-row items-center justify-around m-2'>
-                    <View className='flex items-center justify-evenly bg-white rounded-lg shadow-sm p-2.5'>
+                    <View className='w-5/12 flex items-center justify-evenly bg-white rounded-lg shadow-sm p-2.5'>
                         <Icon size={40} source={require('../assets/call.png')} />
-                        <View className='flex items-center'>
-                            <Text className='font-bold text-base'>477</Text>
-                            <Text>Call survey</Text>
+                        <View className='mt-2 flex items-center'>
+                            <Text className='font-bold text-lg'>477</Text>
+                            <Text className='font-semibold text-base'>Call survey</Text>
                         </View>
                     </View>
 
 
-                    <View className='flex items-center justify-evenly bg-white rounded-lg shadow-sm p-2.5'>
+                    <View className='w-5/12 flex items-center justify-evenly bg-white rounded-lg shadow-sm p-2.5'>
                         <Icon size={40} source={require('../assets/other.png')} />
-                        <View className='flex items-center'>
-                            <Text className='font-bold text-base'>157</Text>
-                            <Text>Other survey</Text>
+                        <View className='mt-2 flex items-center'>
+                            <Text className='font-bold text-lg'>157</Text>
+                            <Text className='font-semibold text-base'>Other survey</Text>
                         </View>
                     </View>
                 </View>

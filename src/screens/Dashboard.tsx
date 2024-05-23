01c853fe1610bfe99131479408b89df2a6d5d210
thejunghare <prasadjunghare2@gmail.com>
2024-05-23 18:59:52 +0530

    add logout button

--- src/screens/Dashboard.tsx
+++ src/screens/Dashboard.tsx
@@ -1,17 +1,43 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 import { useUser } from '../appwrite/UserContext';
-
+import { Button, Icon } from '@rneui/themed';
 
 export default function Dashboard() {
     const user = useUser();
 
-    console.log(user.current);
+    const handleLogout = () => {
+        user.logout(user.current)
+    }
+
+    // console.log(user.current);
     return (
         <View>
-           <Text>
+            <Text>
                 Welcome, {user.current ? user.current.email : 'Please login'}
-           </Text>
+            </Text>
+
+            <Button
+                onPress={
+                    handleLogout
+                }
+                radius={"sm"}
+                type="solid"
+                iconPosition='right'
+                containerStyle={{
+                    width: 200,
+                    marginHorizontal: 50,
+                    marginVertical: 10,
+                }}>
+                Logout
+                <Icon
+                    name="logout"
+                    color="white"
+                    containerStyle={{
+                        marginRight: 10,
+                    }}
+                />
+            </Button>
         </View>
     );
 }

6e639d8bb367b93ce95e8a38c82308837dedb084
thejunghare <prasadjunghare2@gmail.com>
2024-05-24 19:15:45 +0530

    fix type issue

--- src/appwrite/toast.ts
+++ src/appwrite/toast.ts
@@ -1,7 +1,8 @@
-import { ToastAndroid, Platform, } from 'react-native';
+import { ToastAndroid, Platform } from 'react-native';
 
-export function toast(msg) {
+export function toast(msg: string): void {
     if (Platform.OS === 'android') {
-        ToastAndroid.show(msg, ToastAndroid.SHORT)
+        ToastAndroid.show(msg, ToastAndroid.SHORT);
     }
 }
+

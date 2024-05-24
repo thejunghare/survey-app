2a684d6febd9710bd5f67f8215e145a20a61e007
thejunghare <prasadjunghare2@gmail.com>
2024-05-24 19:18:01 +0530

    fix app to support surveyProvider

--- App.tsx
+++ App.tsx
@@ -1,11 +1,14 @@
 import { StyleSheet, Text, View } from 'react-native';
 import { UserProvider } from './src/appwrite/UserContext';
 import { Router } from './src/routes/Router';
+import { SurveyProvider } from './src/appwrite/SurveyContext'; // Add import
 
 export default function App() {
   return (
     <UserProvider>
+      <SurveyProvider>
         <Router />
+      </SurveyProvider>
     </UserProvider >
   );
 }

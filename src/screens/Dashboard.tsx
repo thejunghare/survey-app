ce5c93fa88f4029eb770e9ac37162ca2c21403fe
thejunghare <prasadjunghare2@gmail.com>
2024-06-07 14:41:46 +0530

    - updated ui

--- src/screens/Dashboard.tsx
+++ src/screens/Dashboard.tsx
@@ -1,5 +1,15 @@
 import React, {useContext, useState, useEffect, useCallback} from 'react'
-import {SafeAreaView, View, Text, ActivityIndicator, TouchableOpacity, ScrollView, RefreshControl} from 'react-native';
+import {
+    SafeAreaView,
+    View,
+    Text,
+    ActivityIndicator,
+    TouchableOpacity,
+    ScrollView,
+    RefreshControl,
+    Dimensions,
+    Image
+} from 'react-native';
 import {AppwriteContext} from '../appwrite/UserContext';
 import {Icon, Avatar} from '@rneui/themed';
 import {useNavigation} from '@react-navigation/native';
@@ -15,6 +25,8 @@ type UserObj = {
     email: String;
     id: String;
 }
+const screenWidth = Dimensions.get('window').width
+const screenHeight = Dimensions.get('window').height
 
 const Dashboard: React.FC = () => {
     const {appwrite, setIsLoggedIn} = useContext(AppwriteContext)
@@ -96,9 +108,21 @@ const Dashboard: React.FC = () => {
                 <RefreshControl refreshing={refreshing} onRefresh={onRefresh}/>
             }
         >
-            <SafeAreaView className='bg-white'>
-                <View className='h-screen justify-evenly'>
-                    <View className='h-2/5 justify-evenly items-center bg-slate-200'>
+            <SafeAreaView className='bg-white h-screen justify-evenly'>
+                <ScrollView horizontal={true} className={'h-2/4 content-evenly'}>
+                    <View className='border-r items-center justify-around bg-slate-200'
+                          style={{width: screenWidth}}>
+                        <Image source={require('../../assets/mla-1.png')} style={{width: screenWidth, height: 298}}/>
+
+                        <View className='flex flex-row items-center'>
+                            <Text className=' p-2 rounded font-semibold text-base'>
+                                विश्वनाथ भोईर कल्याण पश्चिम आमदार
+                            </Text>
+                        </View>
+
+                    </View>
+                    <View className='items-center justify-around bg-slate-200 '
+                          style={{width: screenWidth}}>
                         <Avatar
                             size={'xlarge'}
                             rounded
@@ -124,83 +148,88 @@ const Dashboard: React.FC = () => {
                             />
                         </View>
                     </View>
+                </ScrollView>
 
-                    <View className={'h-1/6 items-center justify-center'}>
-                        <Text
-                            className={'bg-app-white m-3 rounded-xl p-3 text-center font-semibold text-base'}>Statistics</Text>
-                    </View>
+                <View className={'h-1/6 mx-3 justify-center'}>
+                    <Text style={{backgroundColor: '#264653'}}
+                          className={'bg-app-white m-3 p-3 text-center text-white rounded font-semibold text-base'}>Statistics</Text>
+                </View>
 
-                    <View className=' h-2/5 justify-start'>
-                        <ScrollView>
-                            <View className='w-full flex flex-row items-start justify-evenly'>
-                                <View
-                                    className='w-5/12 flex items-center justify-evenly bg-app-white rounded-lg shadow-sm p-2.5'>
-                                    <TouchableOpacity>
-                                        <View className="items-center">
-                                            <Icon name='store-outline' type='material-community' size={35}/>
-                                        </View>
-                                        <View className='mt-2 flex items-center'>
-                                            <Text className='font-bold text-lg text-center'>
-                                                0
-                                            </Text>
-                                            <Text className="font-semibold text-base text-center">Shop</Text>
-                                        </View>
-                                    </TouchableOpacity>
-                                </View>
+                <View className='h-2/5 justify-start'>
+                    <ScrollView>
+                        <View className='w-full flex flex-row items-start justify-evenly'>
+                            <View
+                                style={{backgroundColor: '#E76F51'}}
 
-
-                                <View
-                                    className='w-5/12 flex items-center justify-evenly bg-app-white rounded-lg shadow-sm p-2.5'>
-                                    <TouchableOpacity onPress={() => {
-                                        navigation.navigate('Survey form', {userId: userData.id})
-                                    }}>
-                                        <View className='items-center'>
-                                            <Icon name='book-outline' type='material-community' size={35}/>
-                                        </View>
-                                        <View className='mt-2 flex items-center'>
-                                            <Text className='font-bold text-lg text-center'>
-                                                {surveyCount}
-                                            </Text>
-                                            <Text className="font-semibold text-base text-center">Survey</Text>
-                                        </View>
-                                    </TouchableOpacity>
-                                </View>
+                                className='w-5/12 flex items-center justify-evenly text-white rounded shadow-sm p-2.5'>
+                                <TouchableOpacity>
+                                    <View className="items-center">
+                                        <Icon name='store-outline' type='material-community' size={35}/>
+                                    </View>
+                                    <View className='mt-2 flex items-center'>
+                                        <Text className='font-bold text-lg text-center'>
+                                            0
+                                        </Text>
+                                        <Text className="font-semibold text-base text-center">Shop</Text>
+                                    </View>
+                                </TouchableOpacity>
                             </View>
 
-                            <View className='mt-3 w-full flex flex-row items-start justify-evenly'>
-                                <View
-                                    className='w-5/12 flex items-center justify-evenly bg-app-white rounded-lg shadow-sm p-2.5'>
-                                    <TouchableOpacity onPress={() => navigation.navigate('Voter Search')}>
-                                        <View className="items-center">
-                                            <Icon name='account-search-outline' type='material-community' size={35}/>
-                                        </View>
-                                        <View className='mt-2 flex items-center'>
-                                            <Text className='font-bold text-lg text-center'>
-                                                0
-                                            </Text>
-                                            <Text className="font-semibold text-base text-center">Voter Search</Text>
-                                        </View>
-                                    </TouchableOpacity>
-                                </View>
 
-                                <View
-                                    className='w-5/12 flex items-center justify-evenly bg-app-white rounded-lg shadow-sm p-2.5'>
-                                    <TouchableOpacity
-                                    >
-                                        <View className='items-center'>
-                                            <Icon name='chart-box-outline' type='material-community' size={35}/>
-                                        </View>
-                                        <View className='mt-2 flex items-center'>
-                                            <Text className='font-bold text-lg text-center'>
-                                                0
-                                            </Text>
-                                            <Text className="font-semibold text-base text-center">Opinion Poll</Text>
-                                        </View>
-                                    </TouchableOpacity>
-                                </View>
+                            <View
+                                style={{backgroundColor: '#F4A261'}}
+                                className='w-5/12 flex items-center justify-evenly bg-app-white rounded shadow-sm p-2.5'>
+                                <TouchableOpacity onPress={() => {
+                                    navigation.navigate('Survey form', {userId: userData.id})
+                                }}>
+                                    <View className='items-center'>
+                                        <Icon name='book-outline' type='material-community' size={35}/>
+                                    </View>
+                                    <View className='mt-2 flex items-center'>
+                                        <Text className='font-bold text-lg text-center'>
+                                            {surveyCount}
+                                        </Text>
+                                        <Text className="font-semibold text-base text-center">Survey</Text>
+                                    </View>
+                                </TouchableOpacity>
                             </View>
-                        </ScrollView>
-                    </View>
+                        </View>
+
+                        <View className='mt-3 w-full flex flex-row items-start justify-evenly'>
+                            <View
+                                style={{backgroundColor: '#2A9D8F'}}
+                                className='w-5/12 flex items-center justify-evenly bg-app-white rounded shadow-sm p-2.5'>
+                                <TouchableOpacity onPress={() => navigation.navigate('Voter Search')}>
+                                    <View className="items-center">
+                                        <Icon name='account-search-outline' type='material-community' size={35}/>
+                                    </View>
+                                    <View className='mt-2 flex items-center'>
+                                        <Text className='font-bold text-lg text-center'>
+                                            0
+                                        </Text>
+                                        <Text className="font-semibold text-base text-center">Voter Search</Text>
+                                    </View>
+                                </TouchableOpacity>
+                            </View>
+
+                            <View
+                                style={{backgroundColor: '#E9C46A'}}
+                                className='w-5/12 flex items-center justify-evenly bg-app-white rounded shadow-sm p-2.5'>
+                                <TouchableOpacity
+                                >
+                                    <View className='items-center'>
+                                        <Icon name='chart-box-outline' type='material-community' size={35}/>
+                                    </View>
+                                    <View className='mt-2 flex items-center'>
+                                        <Text className='font-bold text-lg text-center'>
+                                            0
+                                        </Text>
+                                        <Text className="font-semibold text-base text-center">Opinion Poll</Text>
+                                    </View>
+                                </TouchableOpacity>
+                            </View>
+                        </View>
+                    </ScrollView>
                 </View>
             </SafeAreaView>
         </ScrollView>
